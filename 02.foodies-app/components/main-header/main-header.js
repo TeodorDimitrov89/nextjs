4b156736a89231c26f5124d10444a189ace24aa8
Teodor Dimitrov <teodordimitrov908@gmail.com>
2024-09-13 16:55:47 +0300

    basic styles

--- 02.foodies-app/components/main-header/main-header.js
+++ 02.foodies-app/components/main-header/main-header.js
@@ -1,26 +1,27 @@
 import Link from "next/link";
 import logoImg from "@/assets/logo.png";
 
-import styles from "./main-header.module.css";
+import classes from "./main-header.module.css";
 import Image from "next/image";
 import MainHeaderBackground from "./main-header-background";
+import NavLink from "./nav-link";
 
 const MainHeader = () => {
   return (
     <>
       <MainHeaderBackground />
-      <header className={styles.header}>
-        <Link href="/" className={styles.logo}>
+      <header className={classes.header}>
+        <Link href="/" className={classes.logo}>
           <Image src={logoImg} alt="A plate with food on it" priority />
           Next Level Food
         </Link>
-        <nav className={styles.nav}>
+        <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href="/meals">Browse Meals</Link>
+              <NavLink href="/meals">Browse Meals</NavLink>
             </li>
             <li>
-              <Link href="/community">Foodies Comminity</Link>
+              <NavLink href="/community">Foodies Comminity</NavLink>
             </li>
           </ul>
         </nav>
