4a5b3d897cdaaa3d910a1da1e492a66f3ccbf590
Teodor Dimitrov <teodordimitrov908@gmail.com>
2024-09-19 15:59:43 +0300

    using parallel routes

--- 03.news-app/app/news/[slug]/page.js
+++ 03.news-app/app/news/[slug]/page.js
@@ -1,13 +1,18 @@
 import { DUMMY_NEWS } from "@/dummy-news";
+import { notFound } from "next/navigation";
 
 const NewsDetailPage = ({ params }) => {
   const newsSlug = params.slug;
   const newsItem = DUMMY_NEWS.find((news) => news.slug === newsSlug);
+
+  if (!newsItem) {
+    notFound();
+  }
   return (
     <article className="news-article">
       <header>
         <img src={`/images/news/${newsItem.image}`} alt={`${newsItem.title}`} />
-        <h1>{newsItem?.title}</h1>
+        <h1>{newsItem.title}</h1>
 
         <time dateTime={newsItem.date}>{newsItem.date}</time>
       </header>
